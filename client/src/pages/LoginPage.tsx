24bb750b5dd22a179b0897de732304f17d6e2b91
LucasBbe <lucas.boube@gmail.com>
2025-02-12 21:35:05 +0100

    Added header, login and register page and all the logo needed in the header

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import "./LoginPage.css";
+
+function LoginPage() {
+  return (
+    <section className="table-page-section">
+      <h1>Page Login</h1>
+    </section>
+  );
+}
+
+export default LoginPage;
---
+++ client/src/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import "./RegisterPage.css";
+
+function RegisterPage() {
+  return (
+    <section className="table-page-section">
+      <h1>Page Register</h1>
+    </section>
+  );
+}
+
+export default RegisterPage;
